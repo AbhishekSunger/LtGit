8ddbd50a27dab088291003aa80a2dc09524d05d9
SURYA KUMAR SUNGER <>
2018-08-18 19:12:35 +0530

    back ground color set to red

---
+++ LtGit/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  LtGit
+//
+//  Created by SURYA KUMAR SUNGER on 18/08/18.
+//  Copyright © 2018 Antispol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        self.view.backgroundColor = UIColor.red
+        
+        
+         
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
